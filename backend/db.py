57f2573a2f18899ea47ef88bdbd11de54f004ef2
Damsara42 <230258519+Damsara42@users.noreply.github.com>
2026-04-05 01:03:25 +0530

    add database connection

---
+++ backend/db.py
@@ -0,0 +1,11 @@
+import pyodbc
+
+def get_connection():
+    conn = pyodbc.connect(
+        'DRIVER={ODCB Driver 17 for SQL Server};' 
+        'SERVER=DESKTOP-D91DH96\SQLEXPRESS;' 
+        'DATABASE=NOVELLA;'
+        'Trusted_Connection = yes;'
+    )
+
+    return conn
